49c05aa01037500a3090d544779723970656e453
Wallace de Freitas <wallace.freitas.b@gmail.com>
2025-02-22 13:05:50 -0300

    feat: add colors and progress enums

--- src/shared/utils/enums.ts
+++ src/shared/utils/enums.ts
@@ -7,3 +7,14 @@ export enum Points {
   MIN_POINTS = 0,
   MAX_POINTS = 6,
 }
+
+export enum Colors {
+  DEFAULT = "#e0e0e0",
+  GRAY = "gray",
+  GREEN = "green",
+}
+
+export enum Progress {
+  EMPTY = 0,
+  FULL = 100,
+}
