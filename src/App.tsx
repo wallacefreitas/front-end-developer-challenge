2592dda191da8838034c2290eb5dbf03d5124357
Wallace de Freitas <wallace.freitas.b@gmail.com>
2025-02-23 09:40:33 -0300

    feat: implement board game and talent provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useContext } from "react";
+import { Container } from "./components/container";
+import { Main } from "./components/main";
+import { Board } from "./components/board";
+import { TalentTree } from "./components/talent-tree";
+import { ScoreCard } from "./components/score-card";
+import { PathGroupComponent } from "./components/path-group/PathGroup";
+import { TalentContext } from "./context/TalentContext";
+import { talentsPath1, talentsPath2 } from "./shared/utils/types";
+import { Points } from "./shared/utils/enums";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const { points } = useContext(TalentContext);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container onContextMenu={(e) => e.preventDefault()}>
+      <Main>
+        <Board.Root>
+          <Board.Header />
+          <Board.Body>
+            <TalentTree.Root>
+              <TalentTree.Group>
+                <TalentTree.Path>
+                  <PathGroupComponent
+                    label={`Talent Path 1`}
+                    talentsPath={talentsPath1}
+                  />
+                </TalentTree.Path>
+              </TalentTree.Group>
+              <TalentTree.Group>
+                <TalentTree.Path>
+                  <PathGroupComponent
+                    label={`Talent Path 2`}
+                    talentsPath={talentsPath2}
+                  />
+                </TalentTree.Path>
+              </TalentTree.Group>
+            </TalentTree.Root>
+            <ScoreCard.Root>
+              <ScoreCard.Content>
+                <span>{`${points}/${Points.MAX_POINTS}`}</span>
+                <span>{`Points Spent`}</span>
+              </ScoreCard.Content>
+            </ScoreCard.Root>
+          </Board.Body>
+        </Board.Root>
+      </Main>
+    </Container>
+  );
 }
-
-export default App
